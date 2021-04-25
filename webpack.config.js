f28c4d974b7f7930c6f77f3be2855c3bdc09b420
iDestar <53664560+iDestar@users.noreply.github.com>
2021-04-25 16:09:20 +0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+
+  devServer: {
+    watchContentBase: true,
+    contentBase: './dist',
+    hot: true,
+  },
+};
